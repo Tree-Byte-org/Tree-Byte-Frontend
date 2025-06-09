fbf0bff10c520b2e12263e8f490d8ace898de27c
Josue19-08 <josuemarin2009@hotmail.com>
2025-06-09 15:58:36 -0600

    feat: add project tokenization UI with farm selection and map

---
+++ app/adopt-flow/page.tsx
@@ -0,0 +1,9 @@
+import { AdoptionFlow } from "@/components/adoption-flow/adoption-flow"
+
+export default function AdoptPage() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-green-50 to-white">
+      <AdoptionFlow />
+    </div>
+  )
+}
