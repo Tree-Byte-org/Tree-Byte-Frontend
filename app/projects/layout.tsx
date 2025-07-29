4769c7f91d1a028bf380a908ac9bcd26d1c17407
Josué Brenes <josuebrenes3005@gmail.com>
2025-07-29 10:31:49 -0600

    feat: implement DashboardLayout and refactor ProjectsPage to use new layout

---
+++ app/projects/layout.tsx
@@ -0,0 +1,14 @@
+import Footer from "@/components/dashboard/footer";
+import Header from "@/components/dashboard/header";
+import FooterProjects from "@/components/projects/project-footer";
+import { ReactNode } from "react";
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="bg-[#FAFDF6] text-gray-900 overflow-x-hidden min-h-screen">
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <FooterProjects />
+    </div>
+  );
+}
