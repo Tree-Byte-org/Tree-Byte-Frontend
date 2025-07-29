c2d3a82686e58889b9d74ef9376023a1367b05fb
Joel Vargas <vermudo.com@gmail.com>
2025-07-29 17:15:51 -0600

    feat(nft-section): update typography and enhance layout with new image positioning for improved visual impact

--- components/home/nft-section.tsx
+++ components/home/nft-section.tsx
@@ -15,7 +15,7 @@ export function NftSection() {
           />
 
           <div>
-            <h2 className="text-4xl lg:text-5xl font-bold text-gray-900 dark:text-white mb-6">
+            <h2 className="text-5xl lg:text-6xl font-bold text-gray-900 dark:text-white mb-6">
               <span className="text-primary">YOU WILL GET A UNIQUE</span>
               <br />
               TreeByte<span className="text-primary"> NFT.</span>
@@ -25,11 +25,18 @@ export function NftSection() {
               species, CO₂ captured and updated photos.
             </p>
 
-            <div className="bg-gradient-to-r from-gray-900 via-primary to-primary p-6 rounded-lg mb-8">
-              <h3 className="text-4xl font-bold text-white mb-2">
-                Transform your{" "}
-                <Image src="/favicon.ico" alt="Tree" width={20} height={20} />{" "}
-                digital footprint into REAL trees.
+            <div className="relative bg-gradient-to-r from-gray-900 via-primary to-primary p-6 rounded-lg mb-8">
+              <div className="absolute right-0 top-0">
+                <Image
+                  className="m-4"
+                  src="/images/logo-black.png"
+                  alt="Tree"
+                  width={70}
+                  height={70}
+                />
+              </div>
+              <h3 className="text-5xl font-bold text-white mb-2">
+                Transform your digital footprint into REAL trees.
               </h3>
             </div>
           </div>
