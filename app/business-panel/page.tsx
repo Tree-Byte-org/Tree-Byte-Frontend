895e1fda3d2e44ad93ec4973307b4285f29eb86d
Joel Vargas <vermudo.com@gmail.com>
2025-07-30 12:00:53 -0600

    feat(business-panel): create BusinessPanel and update page structure with new components

---
+++ app/business-panel/page.tsx
@@ -0,0 +1,11 @@
+import { BusinessPanel } from "@/components/business-panel/BusinessPanel";
+import { FooterSection } from "@/components/home/footer-section";
+
+export default function BusinessPanelPage() {
+  return (
+    <>
+      <BusinessPanel />
+      <FooterSection />
+    </>
+  );
+}
---
+++ components/business-panel/BusinessPanel.tsx
@@ -0,0 +1,18 @@
+export const BusinessPanel = () => {
+  return (
+    <section className="min-h-screen bg-white dark:bg-black">
+      <div className="container mx-auto px-4 my-4">
+        <div className="flex items-center justify-between">
+          <h1 className="text-5xl font-bold">Business Panel</h1>
+
+          <div className="flex items-center gap-4">
+            <h2 className="font-bold text-3xl">Level:</h2>
+            <div className="bg-primary rounded p-4 px-10">
+              <p className="text-4xl font-bold">Rooting</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
