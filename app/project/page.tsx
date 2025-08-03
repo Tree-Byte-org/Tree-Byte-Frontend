feb39728404e282cef6b2e572e077b97641e5bef
pfed-prog <fedorovn19@gmail.com>
2025-08-03 23:49:22 +0300

    token mints; added documentation

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -3,7 +3,7 @@ import ProjectList from "@/components/project/project-list";
 export default function ProjectPage() {
   return (
     <main className="p-8">
-      <h1 className="text-2xl font-bold mb-6">Available Projects</h1>
+      <h1 className="text-2xl font-bold mb-6 text-center">Available Projects</h1>
       <ProjectList />
     </main>
   );
